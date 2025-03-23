824bb16667b6d6ac20792628cc4b7e51e511a1ca
Dawid <dawid.games21@gmail.com>
2025-03-23 21:06:11 +0100

    TEST2

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,12 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) throws Exception {
 
-        System.out.println("DZIEŃ DOBRY");
         Service s = new Service("Poland");
         String weatherJson = s.getWeather("Warsaw");
         Double rate1 = s.getRateFor("EUR");
         Double rate2 = s.getNBPRate();
         // ...
         // część uruchamiająca GUI
-        System.out.println("DZIEŃ DOBRY");
-        System.out.println("DZIEŃ DOBRY");
 
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
